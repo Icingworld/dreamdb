644dfd3d7f151a6073a0633d4d494ec898c0b5c5
Icingworld <984442022@qq.com>
2025-12-19 00:26:20 +0800

    refactor(parser): 重新实现 token 以支持全部 SQL 语句

--- src/parser/token.cpp
+++ src/parser/token.cpp
@@ -4,41 +4,41 @@ namespace dreamdb
 {
 
 Token::Token(TokenType type, const std::string & value, std::size_t line, std::size_t column)
-    : type(type)
-    , value(value)
-    , line(line)
-    , column(column)
+    : type_(type)
+    , value_(value)
+    , line_(line)
+    , column_(column)
 {
 }
 
 TokenType Token::get_type() const noexcept
 {
-    return type;
+    return type_;
 }
 
 const std::string & Token::get_value() const noexcept
 {
-    return value;
+    return value_;
 }
 
 std::size_t Token::get_line() const noexcept
 {
-    return line;
+    return line_;
 }
 
 std::size_t Token::get_column() const noexcept
 {
-    return column;
+    return column_;
 }
 
 bool Token::is_keyword() const noexcept
 {
-    return type >= TokenType::SELECT && type <= TokenType::COLLECTION;
+    return type_ >= TokenType::DB_SELECT && type_ <= TokenType::DB_WITH;
 }
 
 bool Token::is_operator() const noexcept
 {
-    return type >= TokenType::EQUAL && type <= TokenType::MODULO;
+    return type_ >= TokenType::DB_EQUAL && type_ <= TokenType::DB_MODULO;
 }
 
 std::string Token::to_string() const
@@ -46,175 +46,235 @@ std::string Token::to_string() const
     std::string result = "Token(";
     
     // 类型名称
-    switch (type) {
+    switch (type_) {
         // 结束标记
-        case TokenType::EOF_TOKEN:
+        case TokenType::DB_EOF_TOKEN:
             result += "EOF";
             break;
 
         // 关键字
-        case TokenType::SELECT:
+        case TokenType::DB_SELECT:
             result += "SELECT";
             break;
-        case TokenType::INSERT:
+        case TokenType::DB_INSERT:
             result += "INSERT";
             break;
-        case TokenType::DELETE_:
+        case TokenType::DB_DELETE:
             result += "DELETE";
             break;
-        case TokenType::UPDATE:
+        case TokenType::DB_UPDATE:
             result += "UPDATE";
             break;
-        case TokenType::CREATE:
+        case TokenType::DB_CREATE:
             result += "CREATE";
             break;
-        case TokenType::DROP:
+        case TokenType::DB_DROP:
             result += "DROP";
             break;
-        case TokenType::USE:
+        case TokenType::DB_USE:
             result += "USE";
             break;
-        case TokenType::FROM:
+        case TokenType::DB_ALTER:
+            result += "ALTER";
+            break;
+        case TokenType::DB_SHOW:
+            result += "SHOW";
+            break;
+        case TokenType::DB_DESCRIBE:
+            result += "DESCRIBE";
+            break;
+        case TokenType::DB_FROM:
             result += "FROM";
             break;
-        case TokenType::WHERE:
+        case TokenType::DB_WHERE:
             result += "WHERE";
             break;
-        case TokenType::INTO:
+        case TokenType::DB_INTO:
             result += "INTO";
             break;
-        case TokenType::VALUES:
+        case TokenType::DB_VALUES:
             result += "VALUES";
             break;
-        case TokenType::SET:
+        case TokenType::DB_SET:
             result += "SET";
             break;
-        case TokenType::AND:
+        case TokenType::DB_AND:
             result += "AND";
             break;
-        case TokenType::OR:
+        case TokenType::DB_OR:
             result += "OR";
             break;
-        case TokenType::NOT:
+        case TokenType::DB_NOT:
             result += "NOT";
             break;
-        case TokenType::AS:
+        case TokenType::DB_AS:
             result += "AS";
             break;
-        case TokenType::DATABASE:
+        case TokenType::DB_ON:
+            result += "ON";
+            break;
+        case TokenType::DB_OFFSET:
+            result += "OFFSET";
+            break;
+        case TokenType::DB_DATABASE:
             result += "DATABASE";
             break;
-        case TokenType::COLLECTION:
+        case TokenType::DB_COLLECTION:
             result += "COLLECTION";
             break;
-        case TokenType::INDEX:
+        case TokenType::DB_INDEX:
             result += "INDEX";
             break;
-        case TokenType::LIMIT:
+        case TokenType::DB_LIMIT:
             result += "LIMIT";
             break;
-        case TokenType::PRIMARY:
+        case TokenType::DB_PRIMARY:
             result += "PRIMARY";
             break;
-        case TokenType::KEY:
+        case TokenType::DB_KEY:
             result += "KEY";
             break;
-        case TokenType::AUTO_INCREMENT:
+        case TokenType::DB_AUTO_INCREMENT:
             result += "AUTO_INCREMENT";
             break;
-        case TokenType::DEFAULT:
+        case TokenType::DB_DEFAULT:
             result += "DEFAULT";
             break;
+        case TokenType::DB_UNIQUE:
+            result += "UNIQUE";
+            break;
+        case TokenType::DB_IF:
+            result += "IF";
+            break;
+        case TokenType::DB_EXISTS:
+            result += "EXISTS";
+            break;
+        case TokenType::DB_ORDER:
+            result += "ORDER";
+            break;
+        case TokenType::DB_BY:
+            result += "BY";
+            break;
+        case TokenType::DB_ASC:
+            result += "ASC";
+            break;
+        case TokenType::DB_DESC:
+            result += "DESC";
+            break;
+        case TokenType::DB_BETWEEN:
+            result += "BETWEEN";
+            break;
+        case TokenType::DB_IN:
+            result += "IN";
+            break;
+        case TokenType::DB_LIKE:
+            result += "LIKE";
+            break;
+        case TokenType::DB_IS:
+            result += "IS";
+            break;
+        case TokenType::DB_SEARCH:
+            result += "SEARCH";
+            break;
+        case TokenType::DB_USING:
+            result += "USING";
+            break;
+        case TokenType::DB_WITH:
+            result += "WITH";
+            break;
+
         // 标识符和字面量
-        case TokenType::IDENTIFIER:
+        case TokenType::DB_IDENTIFIER:
             result += "IDENTIFIER";
             break;
-        case TokenType::STRING_LITERAL:
+        case TokenType::DB_STRING_LITERAL:
             result += "STRING_LITERAL";
             break;
-        case TokenType::NUMBER_LITERAL:
+        case TokenType::DB_NUMBER_LITERAL:
             result += "NUMBER_LITERAL";
             break;
-        case TokenType::BOOLEAN_LITERAL:
+        case TokenType::DB_BOOLEAN_LITERAL:
             result += "BOOLEAN_LITERAL";
             break;
 
         // 运算符
-        case TokenType::EQUAL:
+        case TokenType::DB_EQUAL:
             result += "=";
             break;
-        case TokenType::NOT_EQUAL:
+        case TokenType::DB_NOT_EQUAL:
             result += "!=";
             break;
-        case TokenType::LESS_THAN:
+        case TokenType::DB_LESS_THAN:
             result += "<";
             break;
-        case TokenType::GREATER_THAN:
+        case TokenType::DB_GREATER_THAN:
             result += ">";
             break;
-        case TokenType::LESS_EQUAL:
+        case TokenType::DB_LESS_EQUAL:
             result += "<=";
             break;
-        case TokenType::GREATER_EQUAL:
+        case TokenType::DB_GREATER_EQUAL:
             result += ">=";
             break;
-        case TokenType::PLUS:
+        case TokenType::DB_PLUS:
             result += "+";
             break;
-        case TokenType::MINUS:
+        case TokenType::DB_MINUS:
             result += "-";
             break;
-        case TokenType::MULTIPLY:
+        case TokenType::DB_MULTIPLY:
             result += "*";
             break;
-        case TokenType::DIVIDE:
+        case TokenType::DB_DIVIDE:
             result += "/";
             break;
-        case TokenType::MODULO:
+        case TokenType::DB_MODULO:
             result += "%";
             break;
 
         // 分隔符
-        case TokenType::COMMA:
+        case TokenType::DB_COMMA:
             result += ",";
             break;
-        case TokenType::SEMICOLON:
+        case TokenType::DB_SEMICOLON:
             result += ";";
             break;
-        case TokenType::DOT:
+        case TokenType::DB_DOT:
             result += ".";
             break;
-        case TokenType::LEFT_PAREN:
+        case TokenType::DB_LEFT_PAREN:
             result += "(";
             break;
-        case TokenType::RIGHT_PAREN:
+        case TokenType::DB_RIGHT_PAREN:
             result += ")";
             break;
-        case TokenType::LEFT_BRACKET:
+        case TokenType::DB_LEFT_BRACKET:
             result += "[";
             break;
-        case TokenType::RIGHT_BRACKET:
+        case TokenType::DB_RIGHT_BRACKET:
             result += "]";
             break;
-        case TokenType::LEFT_BRACE:
+        case TokenType::DB_LEFT_BRACE:
             result += "{";
             break;
-        case TokenType::RIGHT_BRACE:
+        case TokenType::DB_RIGHT_BRACE:
             result += "}";
             break;
+
         // 特殊值
-        case TokenType::NULL_LITERAL:
+        case TokenType::DB_NULL_LITERAL:
             result += "NULL";
             break;
+
         default:
             result += "UNKNOWN";
     }
     
-    if (!value.empty()) {
-        result += ", value='" + value + "'";
+    if (!value_.empty()) {
+        result += ", value='" + value_ + "'";
     }
     
-    result += ", line=" + std::to_string(line) + ", column=" + std::to_string(column) + ")";
+    result += ", line=" + std::to_string(line_) + ", column=" + std::to_string(column_) + ")";
     return result;
 }
 
