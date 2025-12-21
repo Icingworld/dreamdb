47e7cc1ba42873eb1252a85c185c38189b0b7aee
Icingworld <984442022@qq.com>
2025-12-21 13:29:44 +0800

    refactor(parser): 重新实现 Insert 节点

--- src/parser/ast/insert_stmt.cpp
+++ src/parser/ast/insert_stmt.cpp
@@ -7,59 +7,54 @@ namespace dreamdb
 
 InsertStmt::InsertStmt(std::size_t line, std::size_t column)
     : AstNode(AstNodeType::INSERT_STMT, line, column)
-    , table_name("")
-    , column_names()
-    , values()
+    , collection_name_("")
+    , column_names_()
+    , values_()
 {
 }
 
-void InsertStmt::set_table_name(const std::string & table)
+void InsertStmt::set_collection_name(const std::string & collection_name)
 {
-    table_name = table;
+    collection_name_ = collection_name;
 }
 
-const std::string & InsertStmt::get_table_name() const noexcept
+const std::string & InsertStmt::get_collection_name() const noexcept
 {
-    return table_name;
+    return collection_name_;
 }
 
 void InsertStmt::add_column_name(const std::string & column)
 {
-    column_names.push_back(column);
+    column_names_.push_back(column);
 }
 
 const std::vector<std::string> & InsertStmt::get_column_names() const noexcept
 {
-    return column_names;
-}
-
-bool InsertStmt::has_column_names() const noexcept
-{
-    return !column_names.empty();
+    return column_names_;
 }
 
 void InsertStmt::add_value(std::unique_ptr<AstNode> value)
 {
-    values.push_back(std::move(value));
+    values_.push_back(std::move(value));
 }
 
 const std::vector<std::unique_ptr<AstNode>> & InsertStmt::get_values() const noexcept
 {
-    return values;
+    return values_;
 }
 
 std::string InsertStmt::debug_string() const
 {
     std::ostringstream oss;
-    oss << "InsertStmt(table=" << (table_name.empty() ? "<none>" : table_name);
+    oss << "InsertStmt(collection_name=" << (collection_name_.empty() ? "<none>" : collection_name_);
 
-    if (has_column_names()) {
+    if (!column_names_.empty()) {
         oss << ", columns=[";
-        for (std::size_t i = 0; i < column_names.size(); ++i) {
+        for (std::size_t i = 0; i < column_names_.size(); ++i) {
             if (i > 0) {
                 oss << ", ";
             }
-            oss << column_names[i];
+            oss << column_names_[i];
         }
         oss << "]";
     } else {
@@ -67,11 +62,11 @@ std::string InsertStmt::debug_string() const
     }
 
     oss << ", values=[";
-    for (std::size_t i = 0; i < values.size(); ++i) {
+    for (std::size_t i = 0; i < values_.size(); ++i) {
         if (i > 0) {
             oss << ", ";
         }
-        oss << values[i]->debug_string();
+        oss << values_[i]->debug_string();
     }
     oss << "]";
 
