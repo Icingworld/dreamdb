742a37db276d0d94457774de21edbeb1ce30d993
Icingworld <984442022@qq.com>
2026-01-01 10:08:13 +0800

    refactor(planner): 修改 Planner 为 LogicalPlanner

--- src/planner/logical_planner.cpp
+++ src/planner/logical_planner.cpp
@@ -1,11 +1,15 @@
-#include "dreamdb/planner/planner.h"
+#include "dreamdb/planner/logical_planner.h"
 
 #include <stdexcept>
 
 namespace dreamdb
 {
 
-std::unique_ptr<LogicalPlanNode> Planner::plan(const AstNode & ast) const
+LogicalPlanner::LogicalPlanner()
+{
+}
+
+std::unique_ptr<LogicalPlanNode> LogicalPlanner::plan(const AstNode & ast) const
 {
     // 根据抽象语法树的类型，选择对应的计划
     switch (ast.get_type()) {
@@ -20,19 +24,19 @@ std::unique_ptr<LogicalPlanNode> Planner::plan(const AstNode & ast) const
     }
 }
 
-std::unique_ptr<LogicalPlanNode> Planner::plan_select(const SelectStmt & select_stmt) const
+std::unique_ptr<LogicalPlanNode> LogicalPlanner::plan_select(const SelectStmt & select_stmt) const
 {
     (void)select_stmt;
     throw std::runtime_error("Select plan is not implemented");
 }
 
-std::unique_ptr<LogicalPlanNode> Planner::plan_delete(const DeleteStmt & delete_stmt) const
+std::unique_ptr<LogicalPlanNode> LogicalPlanner::plan_delete(const DeleteStmt & delete_stmt) const
 {
     (void)delete_stmt;
     throw std::runtime_error("Delete plan is not implemented");
 }
 
-std::unique_ptr<LogicalPlanNode> Planner::plan_update(const UpdateStmt & update_stmt) const
+std::unique_ptr<LogicalPlanNode> LogicalPlanner::plan_update(const UpdateStmt & update_stmt) const
 {
     (void)update_stmt;
     throw std::runtime_error("Update plan is not implemented");
