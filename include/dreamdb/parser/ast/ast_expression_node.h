85524104877edb6281739f8178f94f3c37156e41
icingworld <984442022@qq.com>
2026-01-04 10:56:14 +0800

    feat(parser): 创建 Column_ref 表达式节点

--- include/dreamdb/parser/ast/ast_expression_node.h
+++ include/dreamdb/parser/ast/ast_expression_node.h
@@ -14,14 +14,11 @@ namespace dreamdb
 enum class AstExpressionNodeType : std::uint8_t
 {
     AST_EXPRESSION_LITERAL,           // 字面量表达式
-    AST_EXPRESSION_IDENTIFIER,        // 标识符表达式
+    AST_EXPRESSION_COLUMN_REFERENCE,  // 列引用表达式
     AST_EXPRESSION_UNARY,             // 一元表达式
     AST_EXPRESSION_BINARY,            // 二元表达式
-    AST_EXPRESSION_FUNCTION_CALL,     // 函数调用表达式
-    AST_EXPRESSION_IN,                // IN 表达式
-    AST_EXPRESSION_LIKE,              // LIKE 表达式
-    AST_EXPRESSION_BETWEEN,           // BETWEEN 表达式
-    AST_EXPRESSION_NULL_CHECK,        // NULL 检查表达式
+    AST_EXPRESSION_FUNCTION_CALL      // 函数调用表达式
+    // 后续如需要，在这里可扩展：CAST | CASE WHEN | SUBQUERY 等表达式
 };
 
 /**
